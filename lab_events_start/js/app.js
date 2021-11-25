c1600cc6cd0a8e506dabf88c7059864b2c5cb2ba
Andrew Calder <andrewcalder8@googlemail.com>
2021-11-25 16:27:03 +0000

    got a delete button that doesn't work

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -12,25 +12,28 @@ const handleFormSubmit = function (e) {
   const category = this.category.value;
   // create a new li for submitted data
   const newListItem = document.createElement('li');
-
+  // create div for title
   const innerTitle = document.createElement('div')
   innerTitle.classList.add('show-title');
   innerTitle.textContent = title;
-
+  // create div for author
   const innerAuthor = document.createElement('div')
   innerAuthor.classList.add('show-author');
   innerAuthor.textContent = author;
-
+  // create div for category
   const innerCategory = document.createElement('div')
   innerCategory.classList.add('show-category');
   innerCategory.textContent = category;
-
+  // add divs as children to li
   newListItem.appendChild(innerTitle);
   newListItem.appendChild(innerAuthor);
   newListItem.appendChild(innerCategory);
+  // add class to new li
   newListItem.classList.add('list-item');
+  newListItem.classList.add('flex');
+  // add new li to the existing ul
   const list = document.querySelector('#reading-list');
   list.appendChild(newListItem);
-
+  // resets form
   this.reset();
 }
